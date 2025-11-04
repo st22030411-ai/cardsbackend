ad0f41dd94c0477bc7cd6083ccbe11a32e5d5b87
lennyjesus19com@gmail.com <lennyjesus19com@gmail.com>
2025-11-03 22:58:39 -0600

    comentario

--- db.js
+++ db.js
@@ -1,10 +1,8 @@
 import dotenv from "dotenv";
 dotenv.config();
-
 import mongoose from "mongoose";
 
-const MONGO_URI = process.env.MONGO_URI || "mongodb+srv://5btics:3s0t1l1n@cluster1.tya41dc.mongodb.net/cards"
-  ;
+const MONGO_URI = process.env.MONGO_URI || "mongodb+srv://5btics:3s0t1l1n@cluster1.tya41dc.mongodb.net/cards";
 
 export const connectDB = async () => {
   try {
